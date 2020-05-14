95afb7eec52e82c0d31ac70e6015710a0e774595
Tiago Marques <tiagofm.profissional@outlook.pt>
2020-05-15 01:16:38 +0200

    build v0.1

---
+++ precache-manifest.fc87c9f4c4b25fc2512de693585e819c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dcb358cb8f89143ed33",
+    "url": "/css/app.8ba84360.css"
+  },
+  {
+    "revision": "080dd95ee1da6f248fb7e80a9c6792a7",
+    "url": "/img/xaml.080dd95e.png"
+  },
+  {
+    "revision": "d90dcd2cc7843698b135477f54a58603",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cba4a757da67cf30a65",
+    "url": "/js/about.b45174f1.js"
+  },
+  {
+    "revision": "4dcb358cb8f89143ed33",
+    "url": "/js/app.45a4238d.js"
+  },
+  {
+    "revision": "9ebb7d3dcda7b1c14e52",
+    "url": "/js/chunk-vendors.505959e2.js"
+  },
+  {
+    "revision": "db20def8ce8f56e962a1ab0b4372ec6a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
